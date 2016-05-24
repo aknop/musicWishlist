10b6d19f477ca37c24b768381d945f74a33ac8fa
Alex <a.knop@kunzleigh.com>
2016-05-24 16:56:17 -0400

    Add a genre, add an artist.

---
+++ MusicLibrary/Controllers/ArtistController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MusicLibrary;
+using MusicLibrary.Models;
+
+namespace MusicLibrary.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private trueEntities db = new trueEntities();
+
+        // GET: Artist
+        public ActionResult Index()
+        {
+            var artistList = (from t in db.artists select new SongsViewModel { SongID = t.id, ArtistName = t.artistName });
+            return View(artistList.ToList());
+        }
+
+        // GET: Artist/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            artist artist = db.artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // GET: Artist/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artist/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,artistName")] artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.artists.Add(artist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(artist);
+        }
+
+        // GET: Artist/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            artist artist = db.artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artist/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,artistName")] artist artist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artist);
+        }
+
+        // GET: Artist/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            artist artist = db.artists.Find(id);
+            if (artist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artist);
+        }
+
+        // POST: Artist/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            artist artist = db.artists.Find(id);
+            db.artists.Remove(artist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MusicLibrary/Controllers/GenreController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MusicLibrary;
+using MusicLibrary.Models;
+namespace MusicLibrary.Controllers
+{
+    public class GenreController : Controller
+    {
+        private trueEntities db = new trueEntities();
+        // GET: Genre
+        public ActionResult Index()
+        {
+            var genreList = (from t in db.genres select new SongsViewModel { SongID = t.id, GenreName = t.genreName });
+            return View(genreList.ToList());
+        }
+
+        // GET: songs/Create
+        public ActionResult Create()
+        {
+            ViewBag.genre_id = new SelectList(db.genres, "id", "genreName");
+            return View();
+        }
+
+        // POST: songs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,genreName")] genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.genres.Add(genre);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            ViewBag.genre_id = new SelectList(db.genres, "id", "genreName", genre.genreName);
+            return View(genre);
+        }
+
+        // GET: songs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            genre genre = db.genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: songs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            genre genre = db.genres.Find(id);
+            db.genres.Remove(genre);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: Artist/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            genre genre = db.genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+        // POST: Artist/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,genreName")] genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+    }
+}
